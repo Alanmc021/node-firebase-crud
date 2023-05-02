cfcb16670cbb11b395e13531ecbeb45cc91eee93
Alan Martins <37884191+Alanmc021@users.noreply.github.com>
2023-05-02 16:29:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Inicializa o Firebase
+const serviceAccount = require('./chave.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://teste-node-senai-default-rtdb.firebaseio.com"
+});
+
+// Cria uma nova pessoa
+app.post('/pessoas', (req, res) => {
+  const pessoa = req.body;
+  admin.database().ref('pessoas').push(pessoa)
+    .then(() => {
+      res.status(201).send('Pessoa criada com sucesso');
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
+
+// Retorna todas as pessoas
+app.get('/pessoas', (req, res) => {
+  admin.database().ref('pessoas').once('value')
+    .then(snapshot => {
+      const pessoas = [];
+      snapshot.forEach(childSnapshot => {
+        const pessoa = childSnapshot.val();
+        pessoa.id = childSnapshot.key;
+        pessoas.push(pessoa);
+      });
+      res.send(pessoas);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
+
+// Retorna uma pessoa por ID
+app.get('/pessoas/:id', (req, res) => {
+  const id = req.params.id;
+  admin.database().ref(`pessoas/${id}`).once('value')
+    .then(snapshot => {
+      const pessoa = snapshot.val();
+      pessoa.id = snapshot.key;
+      res.send(pessoa);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
+
+// Atualiza uma pessoa por ID
+app.put('/pessoas/:id', (req, res) => {
+  const id = req.params.id;
+  const pessoa = req.body;
+  admin.database().ref(`pessoas/${id}`).update(pessoa)
+    .then(() => {
+      res.send('Pessoa atualizada com sucesso');
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
+
+// Remove uma pessoa por ID
+app.delete('/pessoas/:id', (req, res) => {
+  const id = req.params.id;
+  admin.database().ref(`pessoas/${id}`).remove()
+    .then(() => {
+      res.send('Pessoa removida com sucesso');
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
+
+// Inicia o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
